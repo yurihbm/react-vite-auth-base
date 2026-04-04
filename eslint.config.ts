7a711ca18ebbf9b11a091b0926d8c6af185a83e0
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2026-04-04 14:29:34 -0300

    chore(eslint): ignore coverage folder

--- eslint.config.ts
+++ eslint.config.ts
@@ -10,7 +10,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-	globalIgnores(["dist", "./src/routeTree.gen.ts"]),
+	globalIgnores(["coverage", "dist", "./src/routeTree.gen.ts"]),
 	{
 		files: ["**/*.{ts,tsx}"],
 		extends: [
