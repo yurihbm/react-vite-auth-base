1755f25f415ea6758095b8e53fd2306b99fb9a91
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2026-04-04 12:13:50 -0300

    feat(routes): use form at login route

--- src/routes/(auth)/login.tsx
+++ src/routes/(auth)/login.tsx
@@ -1,9 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { LoginForm } from "@src/modules/auth";
+
 export const Route = createFileRoute("/(auth)/login")({
 	component: LoginComponent,
 });
 
 function LoginComponent() {
-	return <div>Hello "/(auth)/login"!</div>;
+	return <LoginForm />;
 }
