99c07eacd3cee07be7a378eb27e27a4e5fd98936
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2026-04-04 10:55:27 -0300

    feat(routes): add login and register routes

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -10,33 +10,53 @@
 
 import { Route as rootRouteImport } from './routes/__root'
 import { Route as IndexRouteImport } from './routes/index'
+import { Route as authRegisterRouteImport } from './routes/(auth)/register'
+import { Route as authLoginRouteImport } from './routes/(auth)/login'
 
 const IndexRoute = IndexRouteImport.update({
   id: '/',
   path: '/',
   getParentRoute: () => rootRouteImport,
 } as any)
+const authRegisterRoute = authRegisterRouteImport.update({
+  id: '/(auth)/register',
+  path: '/register',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const authLoginRoute = authLoginRouteImport.update({
+  id: '/(auth)/login',
+  path: '/login',
+  getParentRoute: () => rootRouteImport,
+} as any)
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/login': typeof authLoginRoute
+  '/register': typeof authRegisterRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/login': typeof authLoginRoute
+  '/register': typeof authRegisterRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
+  '/(auth)/login': typeof authLoginRoute
+  '/(auth)/register': typeof authRegisterRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '/login' | '/register'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '/login' | '/register'
+  id: '__root__' | '/' | '/(auth)/login' | '/(auth)/register'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  authLoginRoute: typeof authLoginRoute
+  authRegisterRoute: typeof authRegisterRoute
 }
 
 declare module '@tanstack/react-router' {
@@ -48,11 +68,27 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexRouteImport
       parentRoute: typeof rootRouteImport
     }
+    '/(auth)/register': {
+      id: '/(auth)/register'
+      path: '/register'
+      fullPath: '/register'
+      preLoaderRoute: typeof authRegisterRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/(auth)/login': {
+      id: '/(auth)/login'
+      path: '/login'
+      fullPath: '/login'
+      preLoaderRoute: typeof authLoginRouteImport
+      parentRoute: typeof rootRouteImport
+    }
   }
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  authLoginRoute: authLoginRoute,
+  authRegisterRoute: authRegisterRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 const queryClient = new QueryClient();
@@ -8,13 +8,41 @@ const queryClient = new QueryClient();
 function RootLayout() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<header className="flex h-16 items-center justify-center">
+			<header className="flex h-16 items-center justify-between px-4">
 				<h1 className="text-xl font-bold">React Vite Auth Base</h1>
+				<nav>
+					<ul className="flex items-center gap-4">
+						<li>
+							<Link
+								to="/"
+								className="text-sm font-medium text-foreground/80 hover:text-foreground"
+							>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="/login"
+								className="text-sm font-medium text-foreground/80 hover:text-foreground"
+							>
+								Login
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="/register"
+								className="text-sm font-medium text-foreground/80 hover:text-foreground"
+							>
+								Register
+							</Link>
+						</li>
+					</ul>
+				</nav>
 			</header>
 			<main className="flex min-h-[calc(100vh-4rem-3rem)] flex-col gap-4 px-4 py-6">
 				<Outlet />
 			</main>
-			<footer className="border-t-foreground flex h-12 items-center justify-center border-t text-xs">
+			<footer className="flex h-12 items-center justify-center border-t border-t-foreground text-xs">
 				Made with ❤️ by Yuri Maciel
 			</footer>
 			<TanStackRouterDevtools />
