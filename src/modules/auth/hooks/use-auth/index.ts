ef397f4c108e6e000eb44fb5b77e4b654b37be25
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2026-04-04 18:08:12 -0300

    feat(auth): add template param for useAuth

--- src/modules/auth/hooks/use-auth/index.ts
+++ src/modules/auth/hooks/use-auth/index.ts
@@ -2,26 +2,31 @@ import type { User } from "@src/modules/users";
 
 import { useGetMe } from "@src/modules/users";
 
+interface AuthenticatedUserResult {
+	/** The authenticated user object. */
+	user: User;
+	/** Indicates that no authentication request is currently pending. */
+	isPending: false;
+	/** Indicates that the user is successfully authenticated. */
+	isAuthenticated: true;
+}
+
+interface UnauthenticatedUserResult {
+	/** The user object is null when not authenticated. */
+	user: null;
+	/** Indicates whether an authentication request is currently in progress. */
+	isPending: boolean;
+	/** Indicates that the user is not authenticated. */
+	isAuthenticated: false;
+}
+
 /**
  * Represents the authentication state returned by the `useAuth` hook.
  */
-type UseAuthResult =
-	| {
-			/** The authenticated user object. */
-			user: User;
-			/** Indicates that no authentication request is currently pending. */
-			isPending: false;
-			/** Indicates that the user is successfully authenticated. */
-			isAuthenticated: true;
-	  }
-	| {
-			/** The user object is null when not authenticated. */
-			user: null;
-			/** Indicates whether an authentication request is currently in progress. */
-			isPending: boolean;
-			/** Indicates that the user is not authenticated. */
-			isAuthenticated: false;
-	  };
+type UseAuthResult<Authenticated extends boolean = false> =
+	Authenticated extends true
+		? AuthenticatedUserResult
+		: AuthenticatedUserResult | UnauthenticatedUserResult;
 
 /**
  * A hook that provides the current authentication status and user information.
@@ -33,6 +38,10 @@ type UseAuthResult =
  * - `isAuthenticated` is only `true` when a valid `user` is available.
  * - Request errors are treated as unauthenticated states.
  * - During request failure, `isPending` is forced to `false`.
+ *
+ * @template Authenticated - A boolean type parameter that indicates whether
+ * the user is authenticated. Useful for private routes where the user must
+ * be authenticated, allowing for more precise typing of the returned object.
  *
  * @returns {UseAuthResult} An object containing the user data, loading state, and authentication status.
  *
@@ -46,7 +55,9 @@ type UseAuthResult =
  * return <div>Welcome, {user.name}!</div>;
  * ```
  */
-export function useAuth(): UseAuthResult {
+export function useAuth<
+	Authenticated extends boolean = false,
+>(): UseAuthResult<Authenticated> {
 	const { data: user, isSuccess, isPending, isError } = useGetMe();
 
 	if (isPending || isError) {
@@ -54,7 +65,7 @@ export function useAuth(): UseAuthResult {
 			user: null,
 			isPending: isError ? false : isPending,
 			isAuthenticated: false,
-		};
+		} as UseAuthResult<Authenticated>;
 	}
 
 	if (isSuccess && user) {
@@ -69,5 +80,5 @@ export function useAuth(): UseAuthResult {
 		user: null,
 		isPending: false,
 		isAuthenticated: false,
-	};
+	} as UseAuthResult<Authenticated>;
 }
